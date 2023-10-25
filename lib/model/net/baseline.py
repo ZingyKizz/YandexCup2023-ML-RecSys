05067fabc77797f6095b17e6234e71ee1a471970
yaroslav.hnykov <yaroslav.hnykov@lamoda.ru>
2023-10-26 02:38:19 +0300

    add projection head

--- lib/model/net/baseline.py
+++ lib/model/net/baseline.py
@@ -36,7 +36,64 @@ class Network(nn.Module):
         return outs
 
 
+class ProjectionHead(nn.Module):
+    def __init__(
+            self, input_dim, hidden_dim, dropout=0.3, residual_connection=True
+    ):
+        super().__init__()
+        self.projection = nn.Linear(input_dim, hidden_dim)
+        self.gelu = nn.GELU()
+        self.fc = nn.Linear(hidden_dim, hidden_dim)
+        self.dropout = nn.Dropout(dropout)
+        self.ln = nn.LayerNorm(hidden_dim)
+        self.residual_connection = residual_connection
+
+    def forward(self, x):
+        projected = self.projection(x)
+        x = self.gelu(projected)
+        x = self.fc(x)
+        x = self.dropout(x)
+        if self.residual_connection:
+            x = x + projected
+        x = self.ln(x)
+        return x
+
+
 class TransNetwork(nn.Module):
+    def __init__(
+            self,
+            num_classes=NUM_TAGS,
+            input_dim=768,
+            hidden_dim=512,
+            n_encoder_layers=2,
+            attention_heads=3,
+    ):
+        super().__init__()
+        self.num_classes = num_classes
+        self.bn = nn.LayerNorm(input_dim)
+        self.mp = MeanPooling()
+        self.encoder_layer = nn.TransformerEncoderLayer(
+            input_dim, attention_heads, input_dim * 4, batch_first=True
+        )
+        self.encoder_block = nn.TransformerEncoder(self.encoder_layer, n_encoder_layers)
+        self.lin = nn.Sequential(
+            nn.Linear(input_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.LayerNorm(hidden_dim),
+        )
+        self.fc = nn.Linear(hidden_dim, num_classes)
+
+    def forward(self, embeds, padding_mask=None):
+        x = self.encoder_block(embeds, src_key_padding_mask=padding_mask)
+        x = self.mp(x, padding_mask=padding_mask)
+        x = self.bn(x)
+        x = self.lin(x)
+        outs = self.fc(x)
+        return outs
+
+
+class TransNetwork1(nn.Module):
     def __init__(
         self,
         num_classes=NUM_TAGS,
@@ -53,12 +110,7 @@ class TransNetwork(nn.Module):
             input_dim, attention_heads, input_dim * 4, batch_first=True
         )
         self.encoder_block = nn.TransformerEncoder(self.encoder_layer, n_encoder_layers)
-        self.lin = nn.Sequential(
-            nn.Linear(input_dim, hidden_dim),
-            nn.ReLU(),
-            nn.Linear(hidden_dim, hidden_dim),
-            nn.LayerNorm(hidden_dim),
-        )
+        self.lin = ProjectionHead(input_dim, hidden_dim)
         self.fc = nn.Linear(hidden_dim, num_classes)
 
     def forward(self, embeds, padding_mask=None):
