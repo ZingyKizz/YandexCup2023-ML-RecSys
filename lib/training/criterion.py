66f1949610ebe9d0ffb81b28b55891ed17510e44
yaroslav.hnykov <yaroslav.hnykov@lamoda.ru>
2023-10-30 22:29:57 +0300

    modify focal loss

--- lib/training/criterion.py
+++ lib/training/criterion.py
@@ -5,16 +5,27 @@ from lib.const import NUM_TAGS
 
 
 class FocalLoss(nn.Module):
-    def __init__(self, gamma=2):
+    def __init__(self, class_weights=None, gamma=2):
         super().__init__()
+        self.class_weights = self.class_weights(class_weights)
         self.gamma = gamma
 
     def forward(self, input, target):
-        logits = input.reshape(-1)
-        targets = target.reshape(-1)
-        p = torch.sigmoid(logits)
-        p = torch.where(targets >= 0.5, p, 1 - p)
+        p = torch.sigmoid(input)
+        p = torch.where(target >= 0.5, p, 1 - p)
         logp = -torch.log(torch.clamp(p, 1e-4, 1 - 1e-4))
         loss = logp * ((1 - p) ** self.gamma)
-        loss = NUM_TAGS * loss.mean()
+        if self.class_weights is not None:
+            loss *= self.class_weights
+        loss = loss.mean()
         return loss
+
+    @staticmethod
+    def _class_weights(class_weights):
+        w = torch.as_tensor(class_weights, dtype=torch.float)
+        w /= torch.sum(w)
+        w *= w.size(0)
+        return w
+
+
+
