0e4cf69b2fdf851141b378989be8b4a614b1be93
yaroslav.hnykov <yaroslav.hnykov@lamoda.ru>
2023-10-31 12:13:39 +0300

    Revert "remova hard block"
    This reverts commit 7e92237380d785100f890fd77c4f60237f01d0ee.

--- lib/model/conv_1d.py
+++ lib/model/conv_1d.py
@@ -69,10 +69,15 @@ class CNN1DModel(nn.Module):
         )
         self.conv_block3 = Conv1dBlock(
             in_channels=4 * in_channels,
-            out_channels=2 * in_channels,
+            out_channels=4 * in_channels,
             skip_connection=True,
         )
         self.conv_block4 = Conv1dBlock(
+            in_channels=4 * in_channels,
+            out_channels=2 * in_channels,
+            skip_connection=True,
+        )
+        self.conv_block5 = Conv1dBlock(
             in_channels=2 * in_channels, out_channels=in_channels, skip_connection=True
         )
         self.pooling = nn.AvgPool1d(kernel_size=(3,), stride=(1,), padding=(1,))
@@ -86,6 +91,8 @@ class CNN1DModel(nn.Module):
         x = self.conv_block3(x)
         x = self.pooling(x)
         x = self.conv_block4(x)
+        x = self.pooling(x)
+        x = self.conv_block5(x)
         x = self.pooling(x)
         x = torch.transpose(x, 1, 2)
         return x
